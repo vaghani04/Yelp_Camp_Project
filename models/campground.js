a2563b633f08a3f128ae9f871604f045bcda6329
Maunik <vaghanimaunik03@gmail.com>
2023-08-31 17:50:05 +0530

    Version 3.2

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -14,5 +15,22 @@ const CampgroundSchema = new Schema({
         }
     ]
 });
+
+// Assuming CampgroundSchema is a mongoose schema, this line sets up a post middleware
+// to be executed after the 'findOneAndDelete' operation on instances of CampgroundSchema.
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    // This block of code is executed only if a document was found and deleted.
+    if (doc) {
+        // This line waits for the asynchronous deletion of reviews associated with the deleted campground.
+        await Review.deleteMany({
+            // This query targets reviews whose '_id' is in the 'reviews' array of the deleted campground.
+            _id: {
+                $in: doc.reviews
+            }
+        });
+    }
+    // End of the post middleware function.
+});
+
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
