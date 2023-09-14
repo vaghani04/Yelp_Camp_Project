f161089cd8d2c4afceb02dc9f078a154df126f27
Maunik <vaghanimaunik03@gmail.com>
2023-09-15 00:01:59 +0530

    Version 4.6

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review');
 
+// https://res.cloudinary.com/drvz6utna/image/upload/w_300/v1694708519/YelpCamp/tent3_bpcgws.jpg
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
