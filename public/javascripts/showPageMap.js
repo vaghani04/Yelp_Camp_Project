62339eee7841f9e9bf7105362df39fb2107dd964
Maunik <vaghanimaunik03@gmail.com>
2023-09-16 17:35:47 +0530

    Version 6.1

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -8,8 +8,10 @@ const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v10',
     center: campground.geometry.coordinates,
-    zoom: 9
+    zoom: 10
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
